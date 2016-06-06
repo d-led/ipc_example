e43903a66a82ff467f953ef11eff4f78dc21ccd5
Dmitry Ledentsov <dmlled@yahoo.com>
2016-06-06 21:35:23 +0200

    reformatted

--- src/example_main/main.cpp
+++ src/example_main/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[]) {
         std::cout << "Sink is: " << sink->identity() << std::endl;
         std::cout << "Source is: " << source->identity() << std::endl;
         sink->send("hi");
-        std::cout<<"received: "<<source->receive()<<std::endl;
+        std::cout << "received: " << source->receive() << std::endl;
 
         const char *n_connection_string = "ipc:///tmp/nreqrep";
         auto n_source = create_nanomsg_source(n_connection_string);
@@ -23,7 +23,7 @@ int main(int argc, char const *argv[]) {
         std::cout << "Sink is: " << n_sink->identity() << std::endl;
         std::cout << "Source is: " << n_source->identity() << std::endl;
         n_sink->send("hi");
-        std::cout<<"received: "<<n_source->receive()<<std::endl;
+        std::cout << "received: " << n_source->receive() << std::endl;
     }
     catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
